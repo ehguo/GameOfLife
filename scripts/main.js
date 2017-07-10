58aa0abfa629a1060eabadd9a911e78576cfe777
ehguo <edward.h.guo@gmail.com>
2017-07-09 20:55:52 -0700

    Added rules/presets divs

--- scripts/main.js
+++ scripts/main.js
@@ -6,9 +6,49 @@ let interval;
 $().ready(onReady);
 
 function onReady() {
+  createHeader();
   createGrid();
   createCells();
   createStart();
+  createPresets();
+}
+
+function createHeader() {
+  const $header = $('<div>', {
+    class: 'header',
+    text: 'The Game of Life'
+  });
+
+  const $ruleInfo = $('<div>', {
+    class: 'rule-info'
+  }).hide()
+    .append($('<div>', {
+      class: 'rule-text',
+      text: '1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.'
+    }))
+    .append($('<div>', {
+      class: 'rule-text',
+      text: '2. Any live cell with two or three live neighbours lives on to the next generation.'
+    }))
+    .append($('<div>', {
+      class: 'rule-text',
+      text: '3. Any live cell with more than three live neighbours dies, as if by overpopulation.'
+    }))
+    .append($('<div>', {
+      class: 'rule-text',
+      text: '4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.'
+    }));
+
+  const $rules = $('<div>', {
+    class: 'rules',
+    text: '-> Rules <-'
+  }).click(handleClick);
+
+  $('#root').append($header).append($ruleInfo).append($rules);
+
+  function handleClick() {
+    $ruleInfo.toggle();
+  }
 }
 
 function createGrid() {
@@ -21,7 +61,7 @@ function createGrid() {
 function createCells() {
   const $grid = $('.grid');
 
-  for (let i = 0; i < 2500; i++) {
+  for (let i = 0; i < 2601; i++) {
     const $cell = $('<div>', {
       id: i,
       class: 'cell'
@@ -44,7 +84,7 @@ function createCells() {
 
 function createStart() {
   const $start = $('<button>', {
-    class: 'start',
+    class: 'button',
     text: 'Start / Stop'
   }).click(runLife);
   $('#root').append($start);
@@ -61,7 +101,7 @@ function runLife() {
 
 function determineFate() {
   const newCellStatus = [];
-  
+
   for (let i = 0; i < cells.length; i++) {
     const adj = getLivingNeighbors(i);
     if (cellStatus[i]) {
@@ -84,7 +124,7 @@ function determineFate() {
 }
 
 function getLivingNeighbors(id) {
-  let rowLen = 50;
+  let rowLen = 51;
   let adj = 0;
 
   if (cellStatus[id - rowLen - 1]) adj++;
@@ -97,4 +137,22 @@ function getLivingNeighbors(id) {
   if (cellStatus[id + rowLen + 1]) adj++;
 
   return adj;
+}
+
+function createPresets() {
+  const $presets = $('<div>', {
+    class: 'presets',
+    text: 'Upcoming Feature!'
+  }).hide();
+
+  const $presetButton = $('<button>', {
+    class: 'button',
+    text: 'Presets'
+  }).click(handleClick);
+
+  $('#root').append($presets).append($presetButton);
+
+  function handleClick(e) {
+    $('.presets').toggle();
+  }
 }
